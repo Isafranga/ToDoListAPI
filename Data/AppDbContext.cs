7d804d561a383fc8894ccf05859f7c8ade629cd1
Isafranga <isadora.a.oliveira6@aluno.senai.br>
2026-03-26 13:51:59 -0300

    feat: Estrutura para as tabelas e migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models.Entities;
+
+namespace ToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configurações adicionais, se necessário
+            modelBuilder.Entity<Usuario>()
+                .HasMany(u => u.Tarefas)
+                .WithOne(t => t.Usuario)
+                .HasForeignKey(t => t.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
+
